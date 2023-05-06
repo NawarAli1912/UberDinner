c8c295769663b1a0c256a878b9ba71e6ff7cc9d5
Nawar <111868983+NawarAli1912@users.noreply.github.com>
2023-05-06 16:39:42 +0300

    setup-global-error-handling

--- UberDinner.Api/Program.cs
+++ UberDinner.Api/Program.cs
@@ -1,15 +1,21 @@
 using UberDinner.Application.Services.Authentication;
 using UberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using UberDinner.Api.Common.Errors;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
     builder.Services.AddInfrastructure(builder.Configuration);
+    
     builder.Services.AddControllers();
+    
+    builder.Services.AddSingleton<ProblemDetailsFactory, UberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
